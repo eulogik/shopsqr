dcf1c470998647a84da7a6c8ebb3de90aecd33d5
eulogik <sumit.khajanchi@eulogik.com>
2016-02-19 18:39:34 +0530

    commit after make rrelation between cart and customer, order and customer

--- common/models/cart.js
+++ common/models/cart.js
@@ -1,3 +1,15 @@
 module.exports = function(Cart) {
 
+	Cart.viewCart = function(data, cb)
+	{
+
+		cb(null, data);
+	}
+	Cart.remoteMethod(
+		"viewCart",
+		{
+			accepts: {arg: 'data', type: 'object', 'http': {source: 'body'}},
+      returns: {arg: 'output', type: 'object', 'http': {source: 'body'}}
+		});
+
 };
